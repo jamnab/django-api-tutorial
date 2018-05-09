9ffedcec03071cedad498adf95531b73d9df9e20
jamnab <jamesbmcnab@gmail.com>
2018-05-09 06:49:22 -0700

    fixed syntax on product model

--- catalog/models.py
+++ catalog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from __future__ import unicode_literals
 from django.db import models
 
-class Product(models.Model);
-  name = models.CharField(max_length=255);
+class Product(models.Model):
+  name = models.CharField(max_length=255)
   description = models.TextField()
   price = models.DecimalField(decimal_places=2, max_digits=20)
