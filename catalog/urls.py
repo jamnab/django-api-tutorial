55afb15c67efbc5fb70d21933d35c04bf21f1796
jamnab <jamesbmcnab@gmail.com>
2018-05-09 18:59:22 -0700

    fixed syntax on urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(r'ˆproducts/$', views.product_list),
+  url(r'^products/$', views.product_list),
 ]
--- drftutorial/urls.py
+++ drftutorial/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'ˆ', include('catalog.urls')),
+    url(r'^', include('catalog.urls')),
 ]
