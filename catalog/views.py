21e9abfe9177708afceda515986fb59e22f944d6
jamnab <jamesbmcnab@gmail.com>
2018-05-21 10:36:50 -0700

    implemented permissions

--- catalog/views.py
+++ catalog/views.py
@@ -3,7 +3,9 @@ from rest_framework import generics
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
+from .permissions import IsAdminOrReadOnly
 
 class ProductList(generics.ListCreateAPIView):
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
+  permission_classes = (IsAdminOrReadOnly, )
