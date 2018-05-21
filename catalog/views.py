66bac3da3dee41615209aca6144d402a9c0f17b1
jamnab <jamesbmcnab@gmail.com>
2018-05-21 11:09:29 -0700

    updated product detail view to generic

--- catalog/views.py
+++ catalog/views.py
@@ -13,27 +13,7 @@ class ProductList(generics.ListCreateAPIView):
   serializer_class = ProductSerializer
   permission_classes = (IsAdminOrReadOnly, )
 
-class ProductDetail(APIView):
-  def get_object(self, pk):
-    try:
-      return Product.objects.get(pk=pk)
-    except Product.DoesNotExist:
-      raise Http404
-  
-  def get(self, request, pk, format=None):
-    product = self.get_object(pk)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
-
-  def put(self, request, pk, format=None):
-    product = self.get_object(pk)
-    serializer = ProductSerializer(product, data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-  def delete(self, request, pk, format=None):
-    product = self.get_object(pk)
-    product.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+class ProductDetail(generics.RetrieveDestroyAPIView):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
+  permission_classes = (IsAdminOrReadOnly, )
